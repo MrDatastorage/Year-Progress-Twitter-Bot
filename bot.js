e034225a7c912afb56e2032288be0d276481c528
Victor Nizeyimana <nizey@yahoo.com>
2018-12-05 08:20:31 +0000

    Calc current perc

--- bot.js
+++ bot.js
@@ -13,10 +13,26 @@ var twitter = require('twit');
 // We need to include our configuration file
 var T = new twitter(require('./config.js'));
 
-// Define the days of a year
+// Define year, and the current year etc.. math..
 var days = 365;
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = now - start;
+var oneDay = 1000 * 60 * 60 * 24;
+var currentDay = Math.floor(diff / oneDay);
+var currentPerc = currentDay / days * 100;
+
+
+console.log('Current day : ' + currentDay);
+console.log('Current perc : ' + currentPerc);
+
 
 // Function
+function postyearProcess(){
+
+
+    
+}
 
 
 // etc...
