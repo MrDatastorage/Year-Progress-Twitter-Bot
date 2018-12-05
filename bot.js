35c1cd54c98e3915fb55da8133e3f033337b4218
Victor Nizeyimana <nizey@yahoo.com>
2018-12-05 08:23:22 +0000

    Some extra notes

--- bot.js
+++ bot.js
@@ -15,14 +15,26 @@ var T = new twitter(require('./config.js'));
 
 // Define year, and the current year etc.. math..
 var days = 365;
+
+// Current Date
 var now = new Date();
+
+// Gets the year, using local time
 var start = new Date(now.getFullYear(), 0, 0);
+
+// Diffrent
 var diff = now - start;
+
+// Calc one day
 var oneDay = 1000 * 60 * 60 * 24;
+
+// Calc current day
 var currentDay = Math.floor(diff / oneDay);
+
+// Calc current passed perc of the year
 var currentPerc = currentDay / days * 100;
 
-
+// Show them in the console
 console.log('Current day : ' + currentDay);
 console.log('Current perc : ' + currentPerc);
 
