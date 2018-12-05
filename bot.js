0303f26faada2b6f4206fe64f5742691cb198a16
Victor Nizeyimana <nizey@yahoo.com>
2018-12-05 08:13:46 +0000

    Twitter Bot first sketch

---
+++ bot.js
@@ -0,0 +1,22 @@
+/**
+* Year Process Twitter Bot in NodeJS
+* This Twitter Bot post the year progress in percents in a tweet.
+*
+* Author Victor Nizeyimanaa
+* Date 05-12-2018
+* License: MIT
+*/
+
+// Our Twitter library
+var twitter = require('twit');
+
+// We need to include our configuration file
+var T = new twitter(require('./config.js'));
+
+// Define the days of a year
+var days = 365;
+
+// Function
+
+
+// etc...
